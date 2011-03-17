51fe11d96ee7eb4532b19647824ae94fffd2da25
Jesse Dearing <jesse.dearing@gmail.com>
2011-03-17 06:36:19 -0500

    Added an admin page

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,8 @@
+class AdminController < ApplicationController
+  def login
+  end
+
+  def authenticate
+  end
+
+end
---
+++ test/functional/admin_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class AdminControllerTest < ActionController::TestCase
+  test "should get login" do
+    get :login
+    assert_response :success
+  end
+
+  test "should get authenticate" do
+    get :authenticate
+    assert_response :success
+  end
+
+end
