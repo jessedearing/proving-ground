6f255a77974b063183eb5e36c76f4e1bb1829a64
Jesse Dearing <jesse.dearing@gmail.com>
2011-03-31 07:31:29 -0500

    Added etc directory location for my site configs

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,3 +1,7 @@
-rcc = YAML.load_file("#{Rails.root}/config/recaptcha.yml")
+if File.exists? "#{Rails.root}/config/recaptcha.yml"
+  rcc = YAML.load_file("#{Rails.root}/config/recaptcha.yml")
+else
+  rcc = YAML.load_file("/etc/jessedearing/recaptcha.yml")
+end
 RCC_PUB = rcc['RCC_PUB']
 RCC_PRIV = rcc['RCC_PRIV']
