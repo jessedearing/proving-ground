084b56a64f376d117857a82e7aab409dc13ffa49
Jesse Dearing <jesse.dearing@gmail.com>
2011-04-30 12:07:36 -0500

    Added some additional nil checks on expiring cache

--- app/concerns/expire_cache.rb
+++ app/concerns/expire_cache.rb
@@ -1,8 +1,8 @@
 module ExpireCache
   def expire_posts_cache(id = nil)
-    id = params[:id] if id.nil?
+    id = params[:id] if id.nil? && params[:id]
     # expire_fragment({:action => 'show', :controller => 'nodes', :id => id})
-    expire_fragment("node#{id}")
+    expire_fragment("node#{id}") unless id.nil?
     expire_fragment({:action => 'index', :controller => 'nodes'})
   end
 end
