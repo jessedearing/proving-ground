be784c92d6b839353843a778d2ea3f698dc129e4
Jesse Dearing <jesse.dearing@gmail.com>
2011-08-26 08:47:15 -0500

    Publish date + results are getting cached
    Wrapped in lambda to fix

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,7 +1,7 @@
 class Node < ActiveRecord::Base
   has_many :comments
   scope :top_posts, limit("#{POSTS_ON_FRONT_PAGE}")
-  scope :published, where("nodes.publish_date < ?", Time.now)
+  scope :published, lambda {where("nodes.publish_date < ?", Time.now)}
 
   def to_param
     "#{id}-#{title.parameterize}"
