14ac3487ec39e8b610e6e40c01dc0c8ffce322fc
Jesse Dearing <jesse.dearing@gmail.com>
2011-11-19 06:28:36 -0600

    Adding linebreaks in comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -17,6 +17,10 @@ class Comment < ActiveRecord::Base
     end
   end
 
+  def body
+    super.gsub("\n", "<br />")
+  end
+
   def update_count
     self.node.comments_count = Comment.complete.to_a.count
     self.node.save
